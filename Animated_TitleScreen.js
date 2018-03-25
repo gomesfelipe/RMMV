f02ee2f404c866ee09499935bcc7dbb6b7abe004
Felipe Gomes <felipe_gomessantos@hotmail.com>
2018-03-25 17:30:00 -0300

    wip

--- Animated_TitleScreen.js
+++ Animated_TitleScreen.js
@@ -6,89 +6,181 @@
  * @author RocketKnight/Felipe Gomes
  */
 
-var Imported = Imported || {};
-var commands =['Menu1','Menu02','Menu03']
-var GomesRocket = GomesRocket || {}; 
-GomesRocket._char_number = Number(GomesRocket.parameters['Char Number'] || 4);
-GomesRocket._char_frame = Number(GomesRocket.parameters['Char Number'] || 4);
-GomesRocket._char_x = Number(GomesRocket.parameters['Char X-Axis'] || 0);
-GomesRocket._char_y = Number(GomesRocket.parameters['Char Y-Axis'] || 0);
 
+/*:
+ * @plugindesc (v1.0) Cena de titulo animado com personagens.
+ * @author Moghunter
 
-Scne_Title.prototype.createCommandWindow = function() {
-	this._commandWindow = new Window_TitleCommand();
-	this._commandWindow.visible = false;
-	this._commandWindow.x = Graphics.Width * 2;
-	this._commandWindow.y = Graphics.height * 2;
-	this._commandWindow.setHandler('newGame',this.commandNewGame.bind(this));
-	this._commandWindow.setHandler('continue',this.commandContinue.bind(this));
-	this._commandWindow.setHandler('options',this.commandOptions.bind(this));
-	this.addWindow(this._commandWindow);
+ * @param Rotation Time
+ * @desc Tempo para ativar a rotação.
+ * @default 300
+ *
+ *
+ */
+
+//=============================================================================
+// ** PLUGIN PARAMETERS
+//=============================================================================
+　　var Imported = Imported || {};
+　　Imported.Animated_TitleScreen = true;
+　　var GomesRocket = GomesRocket || {}; 
+  GomesRocket.parameters = PluginManager.parameters('Animated_TitleScreen');
+
+//=============================================================================
+// ** Scene Title
+//============================================================================= 
+
+//==============================
+// * Initialize
+//==============================
+var _alia_gomes_rocket_initialize = Scene_Title.prototype.initialize;
+Scene_Title.prototype.initialize = function() { 
+    _alia_gomes_rocket_initialize.call(this);
+  this._scenarioField = new Sprite();
+  this._scenarioField.x = Graphics.boxWidth / 2;
+  this._scenarioField.y = Graphics.boxHeight / 2;
+  this._scenarioField.scale.x = 1.00;
+  this._scenarioField.scale.y = this._scenarioField.scale.x;
+  this.addChild(this._scenarioField);
+  this._duration = [[],[],[]];
+  this._side = [0,0,0];
+  this._rt = [0,0,0];
+  this._zt = [1.00,0,0];
+  this._firstRefresh = true;
+  this.loadImages();  
 };
 
-Scene_Title.prototype.createImageCommands = function(){
-	this._imageCommands = new Sprite();
-	this._imageCommands.x = Graphics.Width/2-100;
-	this._imageCommands.y = Graphics.height/2+100;
-	this.addChild(this._imageCommands);
-};
+Scene_Title.prototype.resize = function(){
+ 
+  // Determine which screen dimension is most constrained
+  ratio = Math.min(window.innerWidth/Graphics.width,
+                   window.innerHeight/Graphics.height);
+ 
+  // Scale the view appropriately to fill that dimension
+  stage.scale.x = stage.scale.y = ratio;
+ 
+  // Update the renderer dimensions
+  renderer.resize(Math.ceil(Graphics.width * ratio),
+                  Math.ceil(Graphics.height * ratio));
+}
 
+//==============================
+// * Load Images
+//==============================
 Scene_Title.prototype.loadImages = function() {
-    this._back_img = [ImageManager.loadTitle1("Background_1"),
-                      ];
+  this._back_img = [ImageManager.loadTitle1("layer_1"),
+                     ImageManager.loadTitle1("layer_2"),
+            ];
+
 };
 
-Scene_Title.prototype.createBackground = function() {
-  var width = Graphics.boxWidth * 2;
+//==============================
+// * Create Background
+//==============================
+var _alias_gomesrocket_createBackground = Scene_Title.prototype.createBackground;
+Scene_Title.prototype.createBackground = function() { 
+    _alias_gomesrocket_createBackground.call(this);
+  this.removeChild(this._backSprite1);
+  this.removeChild(this._backSprite2);
+    var width = Graphics.boxWidth * 2;
   var height = Graphics.boxHeight * 2;
   this._background = [];
   for (var i = 0; i < 2 ; i++) {
      this._background[i] = new TilingSprite(this._back_img[i]);
      this._background[i].move(0,0, width, height);
        this._background[i].anchor.x = 0.5;
-     //this._background[i].anchor.y = 0.5;
+     this._background[i].anchor.y = 0.5;
      this._scenarioField.addChild(this._background[i]);
   }
   this._background[1].y = Graphics.boxHeight / 2;
   this._background[1].scale.x = 1.5;
   this._background[1].scale.y = 1.5;
+  this.resize;
+
+//  this.createHimariSprites();
 };
 
-Scene_Title.prototype.createCharacters = function() {
 
+//==============================
+// * First Refresh
+//==============================
+Scene_Title.prototype.firstRefresh = function() {
+   this._firstRefresh = false;
+   for (var i = 0; i < this._char_img.length  ; i++) {  
+        this._chars[0].bitmap = this._char_img[i];
+   };
+   this._background[1].y = Graphics.boxHeight / 2 + (this._back_img[1].height * 18 / 100);
+};
+
+//==============================
+// * Sprite Move To
+//==============================
+Scene_Title.prototype.sprite_move_to = function(value,real_value,speed) {
+  if (value == real_value) {return value};
+  var dnspeed = speed;
+  if (value > real_value) {value -= dnspeed;
+      if (value < real_value) {value = real_value};}
+    else if (value < real_value) {value  += dnspeed;
+      if (value  > real_value) {value  = real_value};   
+    };
+  return value;
+};
+
+//==============================
+// * RefreshSprite
+//==============================
+Scene_Title.prototype.refreshSprite = function(sprite,index,type) {
+  this.setOrder(sprite,index,type);
+    this.setInitialEffect(sprite,index,type);
+};
+
+//==============================
+// * SetOrder
+//==============================
+Scene_Title.prototype.setOrder = function(sprite,index,type) {
+  var d = 0;
+  for (var i = 0; i < this._duration[type].length ; i++) {
+      if (d < this._duration[type][i]) {d = this._duration[type][i]};
+    }; 
+    this._duration[type][index] = d + this.sp_d();
+  this._scenarioField.removeChild(sprite);
+  this._scenarioField.addChild(sprite);
+};
+
+
+//==============================
+// * Update Sprites
+//==============================
+Scene_Title.prototype.updateSprites = function(sprite,index,type) {
+  this._duration[type][index] -= 1
+    if (Math.abs(sprite.scale.x) > 0.2) {sprite.opacity += 10;
+    } else {sprite.opacity -= 25;
   };
+  if (sprite.scale.x < 0) {sprite.scale.x += this.objZoomSpeed(); 
+    } else {sprite.scale.x -= this.objZoomSpeed();
+  };
+  if (type === 1) {
+    if (sprite.scale.x < 0) {sprite.x -= 2} else {sprite.x += 2};
+    };
+  sprite.scale.y = Math.abs(sprite.scale.x);
+  if (this.needRefreshSprite(type,index)) {this.refreshSprite(sprite,index,type)};
 };
 
-Scene_Title.prototype.create = function() {
-    Scene_Base.prototype.create.call(this);
-    this.createBackground();
-    this.createForeground();
-    //this.createWindowLayer();
-   // this.createCommandWindow();
-   this.createImageCommands();
-    this.createCharacters();
+//==============================
+// * Need Refresh Sprite
+//==============================
+Scene_Title.prototype.needRefreshSprite = function(type,index) {
+  if (this._duration[type][index] <= 0) {return true}
+  return false;
 };
-
 
 Scene_Title.prototype.update = function() {
-	this._imageCommands.bitmap = ImageManager.loadSystem(commandsImages[this._commandWindow.index]);
-  this._background[0].origin.x += 1;
-    //if (!this.isBusy()) {
-    //    this._commandWindow.open();
-  //  }
+    this._background[0].origin.x += 1;
+  //this._background[0].origin.y += 1;
+    this._background[1].origin.x += 2; 
+    if (!this.isBusy()) {
+        this._commandWindow.open();
+    }
     Scene_Base.prototype.update.call(this);
-};
-
-Scene_Title.prototype.initialize = function() {
-    this._char_number = Math.min(Math.max(GomesRocket._char_number,1),999);
-    this._char_number += 1;
-    this._char_frame = Math.min(Math.max(GomesRocket._char_number,1),999);
-    this._char_frame += 1;
-    this.loadImages();  
-    Scene_Base.prototype.initialize.call(this);
-    SceneManager.clearStack();
-    this.centerSprite(this._backSprite1);
-    this.centerSprite(this._backSprite2);
-    this.playTitleMusic();
-    this.startFadeIn(this.fadeSpeed(), false);
+    this.resize;
 };
