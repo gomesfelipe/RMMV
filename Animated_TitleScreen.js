e4eefcd06cfa4b4f6e7cd131cf9addbfe61141cb
Felipe Gomes <felipe_gomessantos@hotmail.com>
2018-04-01 13:43:22 -0300

    Update Animated_TitleScreen.js

--- Animated_TitleScreen.js
+++ Animated_TitleScreen.js
@@ -1,5 +1,5 @@
 //=============================================================================
-// Animated_TitleScreen.js
+// BBR_Animated_TitleScreen.js
 //=============================================================================
 /*:
  * @plugindesc (v0.05) Cena de titulo animado.
@@ -8,27 +8,13 @@
 //=============================================================================
 // ** PLUGIN PARAMETERS
 //=============================================================================
- * @param W
- * @type number
- * @desc Setup the button size as pixel unit.
- * (Include the macro string called 'W' and then it replaces as a real value.)
- * @default 78
- * @min 1
  *
- * @param H
- * @type number
- * @desc Setup the button size as pixel unit.
- * (Include the macro string called 'H' and then it replaces as a real value.)
- * @default 78
- * @min 1
- 
   */
 
-　　var Imported = Imported || {};
-　　Imported.Animated_TitleScreen = true;
-　　var GomesRocket = GomesRocket || {}; 
-  GomesRocket.parameters = PluginManager.parameters('Animated_TitleScreen');
-
+　var Imported = Imported || {};
+　Imported.BBR_Animated_TitleScreen = true;
+　var GomesRocket = GomesRocket || {}; 
+  
 //==============================
 // * Initialize
 //==============================
@@ -41,8 +27,10 @@ Scene_Title.prototype.initialize = function() {
   this._scenarioField.scale.x = 1.00;
   this._scenarioField.scale.y = this._scenarioField.scale.x;
   this.addChild(this._scenarioField);
-  this._firstRefresh = true;
+  this._char_number = Math.min(Math.max(GomesRocket.bbr_char_number,1),999);
+  this._char_number += 1;
   this.loadImages();  
+  this.createCharacters();
 };
 
 //==============================
@@ -52,7 +40,10 @@ Scene_Title.prototype.loadImages = function() {
   this._back_img = [ImageManager.loadTitle1("layer_1"),
                      ImageManager.loadTitle1("layer_2"),
             ];
-
+ this._char_img = [];
+  for (var i = 1; i < this._char_number ; i++) {
+       this._char_img.push(ImageManager.loadTitle1("Char_" + i));
+  };
 };
 
 //==============================
@@ -73,11 +64,28 @@ Scene_Title.prototype.createBackground = function() {
      this._background[i].anchor.y = 0.5;
      this._scenarioField.addChild(this._background[i]);
   }
-  this._background[1].y = Graphics.boxHeight / 2;
+  this._background[1].y = Graphics.height / 2;
   this._background[1].scale.x = 1.5;
   this._background[1].scale.y = 1.5;
 };
 
+//==============================
+// * Create Characters
+//==============================
+Scene_Title.prototype.createCharacters = function() {
+  this._char_index = 0;
+  this._chars = [];
+  for (var i = 0; i < 6 ; i++) {
+     this._chars[i] = new Sprite();
+     this._chars[i].x += Graphics.height / 2-100;
+     this._chars[i].y += Graphics.height / 2+100;
+     this._scenarioField.addChild(this._chars[i]);
+
+  };
+  for (var i = 0; i < this._char_img.length  ; i++) {  
+        this._chars[0].bitmap = this._char_img[i];
+   };
+};
 
 Scene_Title.prototype.update = function() {
     this._background[0].origin.x += 1;
