b3dd239da78eb9615eb9e4e0a8220c95710cc1e6
Felipe Gomes <felipe_gomessantos@hotmail.com>
2018-04-01 21:34:05 -0300

    Update BBR_Animated_TitleScreen.js

--- BBR_Animated_TitleScreen.js
+++ BBR_Animated_TitleScreen.js
@@ -34,8 +34,6 @@
 　var GomesRocket = GomesRocket || {}; 
  GomesRocket.parameters = PluginManager.parameters('BBR_Animated_TitleScreen');
  GomesRocket.bbr_char_number = Number(GomesRocket.parameters['Char Number'] || 4);
- var w=Number(GomesRocket.parameters['W']);
- var h=Number(GomesRocket.parameters['H']);
 
 //==============================
 // * Initialize
@@ -43,15 +41,17 @@
 var _alia_gomes_rocket_initialize = Scene_Title.prototype.initialize;
 Scene_Title.prototype.initialize = function() { 
     _alia_gomes_rocket_initialize.call(this);
+  this._char_number = Math.min(Math.max(GomesRocket.bbr_char_number,1),999);
+  this._char_number += 1
   this._scenarioField = new Sprite();
   this._scenarioField.x = Graphics.width / 2;
   this._scenarioField.y = Graphics.height / 2;
   this._scenarioField.scale.x = 1.00;
-  this._scenarioField.scale.y = this._scenarioField.scale.x;
+  this._scenarioField.scale.y = this._scenarioField.scale.x; 
+  
   this.addChild(this._scenarioField);
-  this._char_number = Math.min(Math.max(GomesRocket.bbr_char_number,1),999);
-  this._char_number += 1;
   this.loadImages(); 
+  this.createCharacters(); 
 };
 
 //==============================
@@ -66,6 +66,7 @@ Scene_Title.prototype.loadImages = function() {
        this._char_img.push(ImageManager.loadTitle1("Char_" + i));
   };
   this.createCharacters(); 
+
 };
 
 //==============================
@@ -76,14 +77,17 @@ Scene_Title.prototype.createBackground = function() {
     _alias_gomesrocket_createBackground.call(this);
   this.removeChild(this._backSprite1);
   this.removeChild(this._backSprite2);
+  var width = Graphics.width * 2;
+  var height = Graphics.height * 2;
   this._background = [];
   for (var i = 0; i < 2 ; i++) {
      this._background[i] = new TilingSprite(this._back_img[i]);
+     this._background[i].move(0,0, width, height);
      this._background[i].anchor.x = 0.5;
      this._background[i].anchor.y = 0.5;
      this._scenarioField.addChild(this._background[i]);
   }
-  this._background[1].y = Graphics.boxHeight / 2;
+  this._background[1].y = Graphics.height / 2;
   this._background[1].scale.x = 1.5;
   this._background[1].scale.y = 1.5;
 };
@@ -111,18 +115,19 @@ Scene_Title.prototype.createCharacters = function() {
       }  
      this._chars[i].y = Graphics.height / 2+225;
      this.addChild(this._chars[i]);
-  };
 
+  };
+  
   for (var i = 0; i < this._char_img.length  ; i++) {  
         this._chars[i].bitmap = this._char_img[i];
    };
 };
 
-
 Scene_Title.prototype.update = function() {
     this._background[0].origin.x += 1;
     this._background[0].origin.y = Graphics.height / 2;
     this._background[1].origin.x += 2; 
+   // this.animateCharacters();
     if (!this.isBusy()) {
         this._commandWindow.open();
     }
